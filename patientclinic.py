0704494b4d09047732998ab90a3e6a3f375c950f
wilsjame <jgwilson1214@gmail.com>
2021-04-10 12:56:33 -0700

    get clinic geocodes

--- patientclinic.py
+++ patientclinic.py
@@ -5,15 +5,13 @@ import geopy
 dotenv.load_dotenv()
 g = geopy.geocoders.GoogleV3(api_key=os.environ['GOOGLE_KEY'])
 
-f = open('./data/patients.csv', 'r')
-patient_data = list((f.read().split('\n')))
+with open('./data/patients.csv', 'r') as f:
+    patient_data = list((f.read().split('\n')))
 patient = {}
-
 for line in patient_data:
     a = list(line.split(','))
     if a[0] == 'ID' or a[0] == '':
         continue
-
     assert(len(a) == 6), 'patient address details'
     # ex) 420,5811 BOUL TASCHEREAU BUREAU 100,J4Z 1A5,J4Z,BROSSARD,QC
     ID, address, postal_code, FSA, city, province = map(str, a)
@@ -21,3 +19,19 @@ for line in patient_data:
     location = g.geocode(z)
     geocode = {location.latitude, location.longitude}
     patient[ID] = geocode
+
+with open('./data/clinics.csv', 'r') as f:
+    clinic_data = list(f.read().split('\n'))
+clinic = {}
+for line in clinic_data:
+    a = list(line.split(','))
+    if a[0] == 'Clinic ID' or a[0] == '':
+        continue
+    assert(len(a) == 7), 'clinic address details'
+    # ex) 69,Clinic 69,34 Cedar Pointe Dr Unit 505,L4N 5R7,L4N,Barrie,ON
+    ID, clinic_name, address, postal_code, FSA, city, province = map(str, a)
+    z = ' '.join([address, postal_code, FSA, city, province])
+    location = g.geocode(z)
+    geocode = {location.latitude, location.longitude}
+    clinic[ID] = geocode
+
